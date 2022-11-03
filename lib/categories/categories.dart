0753628f4eb4793239dca6fe5459a434f3040560
David Chaves <dfchaves17@gmail.com>
2022-11-03 17:25:12 -0500

    Integrate with Back End

--- lib/categories/categories.dart
+++ lib/categories/categories.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:gcon_fe/themes/themes.dart';
 import '../controller/categories_controller.dart';
 import '../models/categories_model.dart';
 import '../utils/button_navigator.dart';
@@ -15,7 +16,6 @@ class Categories extends StatefulWidget {
 }
 
 class _CategoriesState extends State<Categories> {
-
   late Future<List<CategoriesModel>> futureCategories;
 
   @override
@@ -36,58 +36,21 @@ class _CategoriesState extends State<Categories> {
           future: futureCategories,
           builder: (context, snapshot) {
             if (snapshot.hasData) {
-
-              return const ScrollViewItems(
-                  restorationId: '', currentList:);
-            } else if (snapshot.hasError) {
-              return Text('${snapshot.error}');
+              var items = snapshot.data;
+              var itemList = <ItemsList>[];
+              for (final i in items!) {
+                itemList.add(ItemsList(
+                    name: i.title, subtitle: i.description, imageUrl: i.image));
+              }
+              return ScrollViewItems(
+                  restorationId: '', currentList: itemList);
             }
-
             // By default, show a loading spinner.
             return const CircularProgressIndicator();
-          },),
-        bottomNavigationBar: const BottomNavBar(currentIndexParam: 1,)
-    );
+          },
+        ),
+        bottomNavigationBar: const BottomNavBar(
+          currentIndexParam: 1,
+        ));
   }
 }
-
-
-const urlPrefix =
-    'https://docs.flutter.dev/cookbook/img-files/effects/parallax';
-const itemList = [
-  ItemsList(
-    name: 'Modelos Gestión del Conocimiento',
-    subtitle: 'Diferentes Modelos de Gestión',
-    imageUrl: 'https://images.unsplash.com/photo-1524995997946-a1c2e315a42f',
-  ),
-  ItemsList(
-    name: 'Conocimiento Tácito',
-    subtitle: "Caracterísiticas del Conocimiento Tácito Y a veces es muy muy largo y no cabe",
-    imageUrl: 'https://images.unsplash.com/photo-1523215122-26803239f41f',
-  ),
-  ItemsList(
-    name: 'Conocimiento Explícito',
-    subtitle: 'Caracterísiticas del Conocimiento Explícito',
-    imageUrl: 'https://images.unsplash.com/photo-1532012197267-da84d127e765',
-  ),
-  ItemsList(
-    name: 'Vitznau',
-    subtitle: 'Switzerland',
-    imageUrl: '$urlPrefix/04-vitznau.jpg',
-  ),
-  ItemsList(
-    name: 'Bali',
-    subtitle: 'Indonesia',
-    imageUrl: '$urlPrefix/05-bali.jpg',
-  ),
-  ItemsList(
-    name: 'Mexico City',
-    subtitle: 'Mexico',
-    imageUrl: '$urlPrefix/06-mexico-city.jpg',
-  ),
-  ItemsList(
-    name: 'Cairo',
-    subtitle: 'Egypt',
-    imageUrl: '$urlPrefix/07-cairo.jpg',
-  ),
-];
